4238b72df423e571f430e42b78e8d6d0076d9b43
Amrit Bastakoti <54471881+Amrit618@users.noreply.github.com>
2022-04-24 12:14:27 +0300

    Updated footer

--- personal-portfolio/src/Component/Footer.js
+++ personal-portfolio/src/Component/Footer.js
@@ -7,6 +7,9 @@ const Footer = () => {
   <h5 className="card-header">Your Choice, My Goal</h5>
   <div className="card-body">
     <h5 className="card-title">Perfect developer for your company</h5>
+        <a href = "https://www.linkedin.com/in/amrit-bastakoti-4a3b471b2/"> Linkedin
+
+    </a>
     <p className="card-text">@2022 Amrit Bastakoti. All rights reserved | Terms and Conditions</p>
     
   </div>
